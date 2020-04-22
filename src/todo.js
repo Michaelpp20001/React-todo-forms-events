8d13f0eee2f70c1bf1d55c513e7e12a704ea924d
Michael <michaelpp2001@gmail.com>
2020-04-22 14:20:57 -0700

    added a button to update state

--- src/todo.js
+++ src/todo.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react'
 
 class Todo extends Component {
-    style = {
-        textAlign: "center"
-    }
-    render () {
-        return (
-            <h1 style={this.style}>Simple Todo App</h1>
-        )
-    }
+    constructor (props) {
+    super(props)
+    this.state = {inputText: "pp"};
 }
 
-export default Todo
+    render () {
+        return (
+            <div className="container">
+                <div className="header">
+                    <h1 style={{textAlign: "center"}}>Simple Todo App</h1>
+                    <p>{this.state.inputText}</p>
+                    <button type="button" onClick={() => {
+                        this.setState((prevState, props) => ({
+                            inputText: prevState.inputText.toUpperCase()
+                        }));
+                    }}>
+                        to UPPERCASE
+                    </button>
+                </div>
+            </div>
+        )
+        }
+}
+
+
+export default Todo;
